a522afdb263e5de88138286661177900901e2eb9
guen <guen5997@naver.com>
2023-10-05 22:39:13 +0900

    🔧wrench: databinding

---
+++ app/src/main/java/com/example/databinding_sample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.databinding_sample
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.databinding_sample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+    }
+}
+
