b04942ed1c63b4ff786ec109dc9e04378e8f2ac3
guen <guen5997@naver.com>
2023-10-08 19:34:04 +0900

    ✨Feat: databinding sample 1

--- app/src/main/java/com/example/databinding_sample/MainActivity.kt
+++ app/src/main/java/com/example/databinding_sample/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.databinding_sample
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.databinding_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var isClicked = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.user = User("kim", "Guen", 15)
+        binding.myActivity = this
+
+        binding.btn.setOnClickListener {
+            isClicked = !isClicked
+        }
 
     }
+}
+
+data class User(val firstName: String, val lastName: String, val age: Int) {
+
 }
 
