0d56796609da1a5c11ac4553a599a76066186078
guen <guen5997@naver.com>
2023-10-08 20:23:48 +0900

    ✨Feat: databinding sample 2 - Handling Method

--- app/src/main/java/com/example/databinding_sample/MainActivity.kt
+++ app/src/main/java/com/example/databinding_sample/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.example.databinding_sample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.databinding_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var isClicked = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
+        val binding: ActivityMainBinding  =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.user = User("kim", "Guen", 15)
-        binding.myActivity = this
-
-        binding.btn.setOnClickListener {
-            isClicked = !isClicked
-        }
-
+        binding.presenter = Presenter()
     }
 }
+
+class Presenter {
+    fun onToastClick(view: View,user: User) {
+        Toast.makeText(view.context, "${user.firstName}, ${user.lastName}", Toast.LENGTH_SHORT).show()
+    }
+}
+
 
 data class User(val firstName: String, val lastName: String, val age: Int) {
 
