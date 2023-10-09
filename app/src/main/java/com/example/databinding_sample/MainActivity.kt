65b49e2b54b6aea8ba6911bf6eb2bf3275803b4f
guen <guen5997@naver.com>
2023-10-09 21:30:01 +0900

    ✨Feat: databinding sample 7 - Basic InverseBindingAdapter

--- app/src/main/java/com/example/databinding_sample/MainActivity.kt
+++ app/src/main/java/com/example/databinding_sample/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.databinding_sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ObservableArrayList
-import androidx.databinding.ObservableArrayMap
 import com.example.databinding_sample.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,26 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        val userList = ObservableArrayList<Any>().apply {
-            add("kim")
-            add("guen")
-            add(30)
-            add("https://github.com/guen-Kim/DataBinding-Basic/assets/69742329/fa838d6a-01b9-492b-bc0f-1ecade3e14c6")
-        }
-        binding.user = userList
-
-        // Update UI
-        CoroutineScope(Main).launch {
-            delay(5000)
-            userList.clear()
-            with(userList) {
-                add("pack")
-                add("guen")
-                add(15)
-                add("https://github.com/guen-Kim/DataBinding-Basic/assets/69742329/fa838d6a-01b9-492b-bc0f-1ecade3e14c6")
-            }
-        }
+        binding.user = User("guen")
 
     }
 }
--- app/src/main/java/com/example/databinding_sample/User.kt
+++ app/src/main/java/com/example/databinding_sample/User.kt
@@ -3,26 +3,14 @@ package com.example.databinding_sample
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 
-data class User1(private var firstName: String, private var lastName: String) : BaseObservable() {
+class User(name: String) : BaseObservable() {
 
     // 데이터 변경 시 알림을 수신 하고자 하는 필드에 Bindable annotation 추가
-    @Bindable
-    fun getFirstName() = firstName
-
-    @Bindable
-    fun getLastName() = lastName
-
-
-    fun setFirstName(firstName: String) {
-        this.firstName = firstName
-        // 데이터 변경을 알린다.
-        notifyPropertyChanged(BR.firstName)
-    }
-
-    fun setLastName(lastName: String) {
-        this.lastName = lastName
-
-        notifyPropertyChanged(BR.lastName)
-    }
-
+    //
+    @get:Bindable
+   var name: String = name
+        set(value) { // custom Setter
+            field = value
+            notifyPropertyChanged(BR.name)
+        }
 }
